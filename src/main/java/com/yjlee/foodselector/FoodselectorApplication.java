411f7ec25319267b608627d988d50b0c6fa3ee52
UD <susan7744850@gmail.com>
2019-12-29 18:12:56 +0900

    Initial commit

---
+++ src/main/java/com/yjlee/foodselector/FoodselectorApplication.java
@@ -0,0 +1,13 @@
+package com.yjlee.foodselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodselectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodselectorApplication.class, args);
+	}
+
+}
