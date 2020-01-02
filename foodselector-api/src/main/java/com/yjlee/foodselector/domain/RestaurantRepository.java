fbc47de171fe1bdcaf3d96c53df87f3e53216edb
UD <susan7744850@gmail.com>
2020-01-02 23:26:44 +0900

    테스트에서 컨트롤러 동작 안하는 경우

--- foodselector-api/src/main/java/com/yjlee/foodselector/domain/RestaurantRepository.java
+++ foodselector-api/src/main/java/com/yjlee/foodselector/domain/RestaurantRepository.java
@@ -1,8 +1,11 @@
 package com.yjlee.foodselector.domain;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class RestaurantRepository {
 
     private List<Restaurant> restaurants = new ArrayList<>();
