dcceee64bdddaa763b9ad452e08db220e6ab18d1
UD <susan7744850@gmail.com>
2020-01-02 23:10:22 +0900

    레파지토리생성

---
+++ foodselector-api/src/main/java/com/yjlee/foodselector/domain/RestaurantRepository.java
@@ -0,0 +1,16 @@
+package com.yjlee.foodselector.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantRepository {
+
+    public List<Restaurant> findAll() {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+
+        return restaurants;
+    }
+}
--- foodselector-api/src/main/java/com/yjlee/foodselector/interfaces/RestaurantController.java
+++ foodselector-api/src/main/java/com/yjlee/foodselector/interfaces/RestaurantController.java
@@ -1,33 +1,30 @@
 package com.yjlee.foodselector.interfaces;
 
 import com.yjlee.foodselector.domain.Restaurant;
+import com.yjlee.foodselector.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
-        List<Restaurant> restaurants = new ArrayList<>();
 
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
+        List<Restaurant> restaurants = repository.findAll();
 
-        restaurants.add(restaurant);
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id){
 
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+        List<Restaurant> restaurants = repository.findAll();
 
         Restaurant restaurant = restaurants.stream()
                 .filter(r->r.getId().equals(id))
