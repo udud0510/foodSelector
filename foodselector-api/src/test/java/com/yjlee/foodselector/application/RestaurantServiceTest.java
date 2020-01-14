20bdd57ce9429963e5b410eac2b6d8516e4e0a52
UD <susan7744850@gmail.com>
2020-01-14 21:31:43 +0900

    MockBean 사용2 ( Test에서 Repository 제거 )

--- foodselector-api/src/test/java/com/yjlee/foodselector/application/RestaurantServiceTest.java
+++ foodselector-api/src/test/java/com/yjlee/foodselector/application/RestaurantServiceTest.java
@@ -1,25 +1,52 @@
 package com.yjlee.foodselector.application;
 
-import com.yjlee.foodselector.domain.*;
+import com.yjlee.foodselector.domain.MenuItem;
+import com.yjlee.foodselector.domain.MenuItemRepository;
+import com.yjlee.foodselector.domain.Restaurant;
+import com.yjlee.foodselector.domain.RestaurantRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
-    private RestaurantRepository restaurantRepository;
     private RestaurantService restaurantService;
+    @Mock
+    private RestaurantRepository restaurantRepository;
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     @Before
     public void setUp() {
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
+        // Mock 연동 및 초기화
+        MockitoAnnotations.initMocks(this);
+        mockRestaurantRepository();
+        mockMenuItemRepository();
         restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
     }
 
     @Test
