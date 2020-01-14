1fbfbdb708100954ff6571f68099984d7490a5af
UD <susan7744850@gmail.com>
2020-01-14 21:14:50 +0900

    MockBean 사용 ( Test에서 Repository 제거 )

--- foodselector-api/src/test/java/com/yjlee/foodselector/interfaces/RestaurantControllerTest.java
+++ foodselector-api/src/test/java/com/yjlee/foodselector/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package com.yjlee.foodselector.interfaces;
 
 import com.yjlee.foodselector.application.RestaurantService;
-import com.yjlee.foodselector.domain.MenuItemRepository;
-import com.yjlee.foodselector.domain.MenuItemRepositoryImpl;
-import com.yjlee.foodselector.domain.RestaurantRepository;
-import com.yjlee.foodselector.domain.RestaurantRepositoryImpl;
+import com.yjlee.foodselector.domain.MenuItem;
+import com.yjlee.foodselector.domain.Restaurant;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,16 +26,17 @@ public class RestaurantControllerTest {
 
     @Autowired
     private MockMvc mvc;
-    // 테스트에 내가 사용할 Repository 선언
-    @SpyBean(RestaurantService.class)
+
+    // 가짜 객체 선언하여 Repositoty 사용 X
+    @MockBean
     private RestaurantService restaurantService;
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository MenuItemRepository;
 
     @Test
     public void list() throws Exception {
+        // 레퍼짓토리를 직접 테스트 하지 않고, 가짜로 테스트 MockBean 사용
+        List<Restaurant> restaurants= new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
 
         mvc.perform(get("/restaurants")).andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
@@ -42,6 +45,14 @@ public class RestaurantControllerTest {
 
     @Test
     public void detail() throws Exception {
+
+        Restaurant restaurant= new Restaurant(1004L,"Bob zip","Seoul");
+        restaurant.addMenuItem(new MenuItem("Kimchi"));
+
+        Restaurant restaurant2= new Restaurant(2020L,"Cyber Food","Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
 
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
